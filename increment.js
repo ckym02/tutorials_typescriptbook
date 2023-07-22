1125003dd9e8414c1caacb0b4f1dff9903337447
koyama-c <koyama@linkedge.jp>
2023-07-23 01:16:02 +0900

    Reactでいいねボタンを作る

--- increment.js
+++
@@ -1,6 +0,0 @@
-// NOTE
-// 「num: number」型注釈(type annotation)
-function increment(num) {
-    return num + 1;
-}
-console.log(increment(999));
---
+++ make-a-simple-function-via-cli/increment.js
@@ -0,0 +1,7 @@
+// NOTE「num: number」型注釈(type annotation)
+function increment(num) {
+    return num + 1;
+}
+console.log(increment(999));
+// tsc increment.ts でコンパイル
+// コンパイラが生成した increment.js をデプロイする

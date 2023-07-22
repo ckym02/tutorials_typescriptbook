3f76a899fd7763594acd389b55ca688045a5e32b
koyama-c <koyama@linkedge.jp>
2023-07-23 03:59:34 +0900

    猫画像ジェネレータを作る
    SSRを行う

--- nextjs/random-cat/pages/index.tsx
+++ nextjs/random-cat/pages/index.tsx
@@ -1,73 +1,62 @@
-import { NextPage } from "next";
-import { useEffect, useState } from "react";
+import { GetServerSideProps, NextPage } from "next";
+import { useState } from "react";
+import styles from "./index.module.css";
+
+// getServerSidePropsから渡されるpropsの型
+type Props = {
+  initialImageUrl: string;
+};
+
+// サーバーサイドで実行する処理
+// Next.jsに認識させるためにexportしておく必要がある
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const image = await fetchImage();
+  console.log("サーバーサイドで実行")
+  return {
+    props: {
+      initialImageUrl: image.url,
+    },
+  };
+};
 
 type Image = {
   url: string;
 };
 
-// 型ガード関数
-const isImage = (value: unknown): value is Image => {
-  // 値がオブジェクトなのか？
-  if (!value || typeof value !== "object") {
-    return false;
-  }
-
-  // urlプロパティが存在し、かつ、それが文字列なのか？
-  return "url" in value && typeof value.url === "string";
-};
-
 const fetchImage = async (): Promise<Image> => {
   const res = await fetch("https://api.thecatapi.com/v1/images/search");
-  const images: unknown = await res.json();
-
-  // 配列として表現されているか？
-  if (!Array.isArray(images)) {
-    throw new Error("猫の画像が取得できませんでした");
-  }
-
-  const image: unknown = images[0];
-  // Imageの構造をなしているか？
-  if (!isImage(image)) {
-    throw new Error("猫の画像が取得できませんでした");
-  }
-
-  return image;
+  const images = await res.json();
+  console.log(images);
+  return images[0];
 };
 
-// NOTE: NextPageはページコンポーネントを表す型
-// ページコンポーネントの要件を満たしているかがチェックする
-const IndexPage: NextPage = () => {
-  const [imageUrl, setImageUrl] = useState('')
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    fetchImage().then((image) => {
-      setImageUrl(image.url)
-      setLoading(false)
-    })
-  }, [])
-
-  // 読み込み中は前の画像が表示されたまま
-  // const handleClick = () => {
-  //   fetchImage().then((image) => {
-  //     setImageUrl(image.url)
-  //   })
-  // }
-
-  // チュートリアルのほう（読み込み中は画面に何も表示されない）
+// ページコンポーネント関数にpropsを受け取る引数を追加する
+// getServerSidePropsが返すpropsを受け取る
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
+  const [imageUrl, setImageUrl] = useState(initialImageUrl); // 初期値を渡す
+  const [loading, setLoading] = useState(false); // 初期状態はfalseにしておく
+  // useEffect(() => {
+  //   fetchImage().then((newImage) => {
+  //     setImageUrl(newImage.url);
+  //     setLoading(false);
+  //   });
+  // }, []);
   const handleClick = async () => {
-    setLoading(true); // 読込中フラグを立てる
+    setLoading(true);
     const newImage = await fetchImage();
-    setImageUrl(newImage.url); // 画像URLの状態を更新する
-    setLoading(false); // 読込中フラグを倒す
-  }
+    setImageUrl(newImage.url);
+    setLoading(false);
+  };
 
   return (
-    <div>
-      <button onClick={handleClick}>他のネコも見る</button>
-      <div>{loading || <img src={imageUrl} />}</div>
+    <div className={styles.page}>
+      <button onClick={handleClick} className={styles.button}>
+        他のにゃんこも見る
+      </button>
+      <div className={styles.frame}>
+        {loading || <img src={imageUrl} className={styles.img} />}
+      </div>
     </div>
-  )
-}
-
+  );
+};
 export default IndexPage;

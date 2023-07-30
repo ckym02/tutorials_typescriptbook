298df64fd2731e7370a90f14049e764c4cfbac67
koyama-c <koyama@linkedge.jp>
2023-07-31 00:15:46 +0900

    ESLintの設定ファイルを追加

--- eslint-tutorial/.eslintrc.js
+++ eslint-tutorial/.eslintrc.js
@@ -1,5 +1,7 @@
 module.exports = {
+  // これ以上ディレクトリを遡らない
   root: true,
+  // JavaScript/TypeScriptコードがどの実行環境で使われるか
   env: {
     browser: true,
     es2021: true,
@@ -7,5 +9,16 @@ module.exports = {
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
+  },
+  // Airbnbの設定を使用するためコメントアウト
+  // rules: {
+  //   "no-console": "error",
+  //   camelcase: ["error", { properties: "never" }],
+  //   semi: ["error", "always"],
+  // },
+  extends: ["airbnb-base"],
+  rules: {
+    "import/prefer-default-export": "off",
+    quotes: ["error", "double"],
   },
 };
---
+++ eslint-tutorial/src/helloWorld.js
@@ -0,0 +1,7 @@
+// export const helloWorld = "Hello World";
+
+// 自動修正コマンド
+// npx eslint src --fix
+
+export const hello_world = "Hello World";
+console.log(hello_world);
